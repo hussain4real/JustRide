c329e52f6df5c5f34366771893b0667473d630a4
Hussain Aminu <hussain_aminu@yahoo.com>
2023-04-19 09:50:16 +0300

    (): setup login and authentication for the user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,11 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -31,7 +27,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'login_code',
         'remember_token',
     ];
 
@@ -44,6 +40,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function routeNotificationForTwilio()
+    {
+        return $this->phone;
+    }
     public function driver(): HasOne
     {
         return $this->hasOne(Driver::class);
